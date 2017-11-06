23793b6511de1fdc1b486eff417f698f002235c0
helenmg <elenamg31@gmail.com>
2017-11-06 13:23:19 +0100

    sinatrapp2

--- app.rb
+++ app.rb
@@ -1,26 +1,43 @@
 require 'rubygems'
 require 'sinatra'
 
+invitaciones = []
+
 get '/' do
   erb :home
 end
 
-get '/invitaciones' do
+post '/invitacion' do
   novio = params["name1"] 
   novia = params["name2"]
   fecha = params["date"]
-  @invitacion = Invitaciones.new(novio, novia, fecha)
-  erb :invitaciones
+  id = invitaciones.count
+  @invitacion = Invitaciones.new(novio, novia, fecha, id)
+
+  invitaciones << @invitacion
+
+  erb :invitacion
 end  
 
+get '/invitacion/:id' do
+  id = params["id"].to_i
+  @invitacion = invitaciones[id]
+  erb :invitacion
+end
+
+get '/lista' do 
+  @invitaciones = invitaciones
+    erb :lista  
+end
 
 class Invitaciones
-  attr_reader :novio, :novia, :fecha
+  attr_reader :novio, :novia, :fecha, :id
 
-  def initialize(novio, novia, fecha)
+  def initialize(novio, novia, fecha, id)
     @novio = novio.capitalize #varaible de instancia
     @novia = novia.capitalize
     @fecha = fecha
+    @id = id
   end
 end
 
