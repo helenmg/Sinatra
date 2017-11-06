4b63d90e1c6f68804ce2a5ebf44f7681f2b57d07
helenmg <elenamg31@gmail.com>
2017-11-06 12:02:29 +0100

    sinatrapp

--- app.rb
+++ app.rb
@@ -1,19 +1,18 @@
 require 'rubygems'
 require 'sinatra'
 
-#class App < Sinatra::Base 
-  get '/' do
-    erb :home
-  end
-  
-  get '/invitaciones' do
-    novio = params["name1"] 
-    novia = params["name2"]
-    fecha = params["date"]
-    @invitacion = Invitaciones.new(novio, novia, fecha)
-    erb :invitaciones
-  end  
-#end
+get '/' do
+  erb :home
+end
+
+get '/invitaciones' do
+  novio = params["name1"] 
+  novia = params["name2"]
+  fecha = params["date"]
+  @invitacion = Invitaciones.new(novio, novia, fecha)
+  erb :invitaciones
+end  
+
 
 class Invitaciones
   attr_reader :novio, :novia, :fecha
